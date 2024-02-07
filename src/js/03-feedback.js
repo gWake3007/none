38f603718c971083c0b2a19bee8ac86ecc186345
gWake3007 <81764484+gWake3007@users.noreply.github.com>
2024-02-07 18:57:11 +0100

    1

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,30 +1,39 @@
-import _ from 'lodash.throttle';
+import _ from 'lodash';
 
-console.log("aaa");
 const localStorageKey = 'feedback-form-state';
 
 const form = document.querySelector('.feedback-form');
 
-try{
-    const objectStorage = JSON.parse(localStorage.getItem(localStorageKey));
-} catch(error) {
-    console.log(error.name);
+const objectStorage = JSON.parse(localStorage.getItem(localStorageKey));
+
+if (objectStorage && Object.keys(objectStorage).length > 0) {
+  form.elements.email.value = objectStorage.email;
+  form.elements.message.value = objectStorage.message;
 }
 
-// form.elements.email.value = objectStorage.email;
-// form.elements.message.value = objectStorage.message;
-
 function inputText() {
-    const objectLocal = JSON.parse(localStorage.getItem(localStorageKey)) ?? {email:"",message:"",};
-    objectLocal.email = form.elements.email.value;
-    objectLocal.message = form.elements.message.value;
-    localStorage.setItem(localStorageKey, JSON.stringify(objectLocal));
+  const objectLocal = JSON.parse(localStorage.getItem(localStorageKey)) ?? {
+    email: '',
+    message: '',
+  };
+  objectLocal.email = form.elements.email.value;
+  objectLocal.message = form.elements.message.value;
+  localStorage.setItem(localStorageKey, JSON.stringify(objectLocal));
 }
 
 form.addEventListener('input', _.throttle(inputText, 500));
 
-// form.addEventListener('submit', submitForm);
+form.addEventListener('submit', submitForm);
 
-// function submitForm() {
-//     localStorage.removeItem(localStorageKey);
-// }
+function submitForm(event) {
+    try{
+        const localStorageCLG = JSON.parse(localStorage.getItem(localStorageKey));
+        console.log(localStorageCLG);
+    } catch(error) {
+        console.log(error);
+    }
+  event.preventDefault();
+  localStorage.removeItem(localStorageKey);
+  form.elements.email.value = "";
+  form.elements.message.value = "";
+}
